22257b4bb7fc369d2a200355d9f884e8918af383
jerinjose259 <58581253+jerinjose259@users.noreply.github.com>
2019-12-06 11:13:28 +0530

    Add files via upload

---
+++ page-contact.php
@@ -0,0 +1,14 @@
+<?php
+get_header();
+?>
+<div class="cont">
+<form>
+<input type="text" class="frm" name="name" placeholder="enter the name">Name</input>
+<input type="text" class="frm" name="email" placeholder="enter the email">Email</input>
+<input type="text" class="frm" name="phone" placeholder="enter the number">Phone</input>
+<textarea placeholder="type your message" class="frm"></textarea>
+<input type="submit" value="send" class="frm-btn"></input>
+</div>
+<?php
+get_footer();
+?>
